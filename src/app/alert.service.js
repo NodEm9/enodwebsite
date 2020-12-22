58858c78994744c7b1ffdc6689848a7b71dee2f7
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2020-12-23 00:19:52 +0100

    created an alert message function

---
+++ src/app/alert.service.js
@@ -0,0 +1,23 @@
+
+function alertMessage() {
+          const html = document.querySelector('html');
+
+          const panel = document.createElement('div');
+          panel.setAttribute('class', 'msgBox');
+          html.appendChild(panel);
+          
+          const msg = document.createElement('p');
+          msg.textContent = 'This is a message';
+          panel.appendChild(msg);
+
+          const closeBtn = document.createElement('button');
+          closeBtn.textContent = 'x';
+          panel.appendChild(closeBtn);
+
+          closeBtn.onclick = function() {
+                    panel.parentNode.removeChild(panel);
+          }
+}
+alertMessage(); 
+
+module.exports = alertMessage;
