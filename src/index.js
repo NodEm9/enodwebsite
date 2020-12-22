8de68d643118b97c3c8e14852e6ec419f1efdf50
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2020-12-23 00:09:30 +0100

    imported all scss files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import { run } from './app/app';
-import './style/main.scss';
-import { HomePage } from './app/home';
+import { homePage } from './app/home';
 import { ComponentService } from './app/component.service';
-import { ContactPage } from './app/contact';
+import { contactPage } from './app/contact';
 import { AboutMe } from './app/about';
+import { alertMessage } from './app/alert.service';
 
-
-
+import './style/main.scss';
+import './style/contact.scss';
+import './style/about.scss';
+import './style/project.scss';
 
 const componentService = new ComponentService();
-const homepage = new HomePage();
-const contactpage = new ContactPage();
+// const homepage = new HomePage();
+// const contactpage = new ContactPage();
 const aboutme = new AboutMe();
 
+console.log(alertMessage)
 
-run(ComponentService, HomePage, ContactPage, AboutMe);
+export {
+          homePage,
+          contactPage,
+          alertMessage
+}
+
+run(ComponentService, AboutMe);
 
