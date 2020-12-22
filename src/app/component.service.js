6defe764d5c4312cb2c56a7a99d156a8534b561b
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2020-12-22 19:14:28 +0100

    create componentService

--- src/app/component.service.js
+++ src/app/component.service.js
@@ -1,37 +1,6 @@
-import { run } from './app';
-import { ContactPage } from './contact';
-
 
 export  class ComponentService {
-   constructor(){
-       const inputGroup = document.querySelector('.input-group');
-       const form = inputGroup.querySelectorAll('.form');
-       const  submitBtn = form[0].querySelector('button[type="submit"]');
-
-       let showForm = [];
-          function getDataForm(e) {  
-             e.preventDefault(); 
-             let showForm = {
-                id: document.body.getElementsByTagName('label'),
-                label: document.querySelectorAll('input-group'), 
-             }
-             //Collects all form data without having to loop through 
-             const formData = new FormData(form[0]);
-          
-             console.log(formData.get('fname')+ ' - '+ formData.get('fullnameField')+ ' - '+ formData.get('emailField')+ ' - '+ 
-             formData.get('subjectTitleField')+ ' - '+ formData.get('messageField'));  
-             
-             showForm
-          }
-          document.querySelector('form').reset();
-          
-          document.addEventListener('DOMContentLoaded', function(){
-          
-                    submitBtn.addEventListener('click', getDataForm, false);
-                 
-             }, false); 
-
-             } 
+   constructor(){}
              
 };  
 
