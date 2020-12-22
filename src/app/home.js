03792bec1fabc0eb875219d180ce42ef5b3c7eee
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2020-12-22 18:48:47 +0100

    create and exported homepae class

--- src/app/home.js
+++ src/app/home.js
@@ -1,11 +1,8 @@
- 
+
 export class HomePage {
     constructor () {
-        let home = document.querySelector('.home');
+        const home = document.querySelector('.home');
         let p = home.querySelector('p');
-        p.innerHTML = '<strong>Emmanuel</strong> Designs Webapp And Mobileapp';
-        let h4 = document.querySelector('h4');
-        h4.innerHTML = `<strong>Contacts:</strong>`;  
-        
+        p.innerHTML = '<strong>Emmanuel</strong> Designs Webapp And Mobileapps';     
  }
 }   
