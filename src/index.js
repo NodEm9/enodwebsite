a17ee6eb19554ecca81280caee22406643fb162c
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2020-12-22 18:36:45 +0100

    imported classes from js files and called run

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-window.$ = require('jquery');
 import { run } from './app/app';
 import './style/main.scss';
 import { HomePage } from './app/home';
 import { ComponentService } from './app/component.service';
 import { ContactPage } from './app/contact';
+import { AboutMe } from './app/about';
+
+
+
 
 const componentService = new ComponentService();
 const homepage = new HomePage();
 const contactpage = new ContactPage();
+const aboutme = new AboutMe();
 
 
+run(ComponentService, HomePage, ContactPage, AboutMe);
 
-run(ComponentService, HomePage, ContactPage);
