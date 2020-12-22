b5738b8782c119fc6954c2411fc80745f3f95e04
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2020-12-22 19:08:45 +0100

    create about.js entry point

---
+++ src/app/about.js
@@ -0,0 +1,11 @@
+
+export class AboutMe  {
+      constructor(){
+         
+         let aboutme = document.querySelector('#bio');
+
+         aboutme.textContent = "My name is Emmanuel";
+ }
+}
+
+
