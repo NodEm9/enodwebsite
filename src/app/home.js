399d61292939ceeb1e7f33c4d81aa076d0183a59
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2020-12-23 00:10:05 +0100

    imported home.scss file directly inside the js file

--- src/app/home.js
+++ src/app/home.js
@@ -1,8 +1,11 @@
 
-export class HomePage {
-    constructor () {
+const homePage = () => {
+   
         const home = document.querySelector('.home');
         let p = home.querySelector('p');
         p.innerHTML = '<strong>Emmanuel</strong> Designs Webapp And Mobileapps';     
  }
-}   
+ homePage();
+ 
+ module.exports = homePage;
+  
