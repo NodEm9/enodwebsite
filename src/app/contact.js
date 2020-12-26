fe15a22245bfcd7b0a356e9073a711098aa1090f
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2020-12-26 02:35:18 +0100

    modified header1 text content

--- src/app/contact.js
+++ src/app/contact.js
@@ -1,13 +1,30 @@
-// import { bootstrap } from 'bootstrap';
+import { bootstrap } from 'bootstrap';
 import '../style/main.scss';
 import '../style/contact.scss';
+import { openNewpage } from './home';
+
+const greetingMsg = document.querySelector('.greetingMsg');
 
 const contactPage = () => {
-          const greetingMsg = document.querySelector('.greetingMsg')
-          const cHeader = greetingHeader.querySelector('h1'); 
-          cHeader.textContent = 'Welcome To My Contact Page';       
- }
+
+          const cHeader = greetingMsg.querySelector('h1'); 
+          cHeader.textContent = 'Welcome to Contacts';
+          const cHeader1 = document.querySelector('.c-heading1');
+          cHeader1.textContent = `Reach us today, with any contact medium convinient to you\n 
+                                    In case we are not available at the moment, Kindly drop us a message
+                                    and we will  endeavour to get back to you ASAP. Thank you. `;                                    
+ 
+          
+                                    
+          const gHpage = document.querySelector('.social__icon');
+
+          gHpage.onclick = function() {
+                    openNewpage(); 
+  }
+}
  contactPage();
 
+
+ 
  export { contactPage }; 
 
