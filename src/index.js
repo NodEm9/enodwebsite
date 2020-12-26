8f8cf763ccca543b1fbabd82039e77b6ac548171
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2020-12-26 02:31:38 +0100

    imported new function form home.js and add comments

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
+//Importing all functions of the siblings files
 import { run } from './app/app';
 import { homePage } from './app/home';
+import { openNewpage } from './app/home';
 import { ComponentService } from './app/component.service';
 import { contactPage } from './app/contact';
 import { AboutMe } from './app/about';
 import { alertMessage } from './app/alert.service';
 
+//importing all scss files 
 import './style/main.scss';
 import './style/contact.scss';
 import './style/about.scss';
 import './style/project.scss';
 
 const componentService = new ComponentService();
-// const homepage = new HomePage();
-// const contactpage = new ContactPage();
 const aboutme = new AboutMe();
-
+ 
 console.log(alertMessage)
 
 export {
           homePage,
+          openNewpage,
           contactPage,
-          alertMessage
+          alertMessage,
 }
 
 run(ComponentService, AboutMe);
