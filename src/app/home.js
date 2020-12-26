f982d93d31799ce0958daf5a21fa77b4829b8827
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2020-12-26 02:33:22 +0100

    intiated a new function to open a new window when gitHub icon is clicked  and add comments

--- src/app/home.js
+++ src/app/home.js
@@ -1,11 +1,29 @@
 
-const homePage = () => {
-   
+const homePage =() => {
         const home = document.querySelector('.home');
         let p = home.querySelector('p');
-        p.innerHTML = '<strong>Emmanuel</strong> Designs Webapp And Mobileapps';     
+        p.innerHTML = 'Web Development, Single-page and complex website design.';
  }
  homePage();
- 
- module.exports = homePage;
+
+
+ //Select the DOM element for GitHub
+ const socialIcon = document.querySelector('.social__icon');
+
+ //Add an eventListener with the function 
+ socialIcon.addEventListener('click', openNewpage);
+
+ //Run this function if the icon is clicked
+ function openNewpage(evt) {
+         evt.preventDefault();
+
+         if(socialIcon) {
+               window.open('https://github.com/NodEm9');
+               return
+         }
+ }
+ openNewpage();
+
+ //Make sure all functions are exported correctly
+ export { homePage, openNewpage };
   
