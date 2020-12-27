98f28895cc6d500e03888beb80d43f58bdc73366
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2020-12-28 00:23:30 +0100

    move all shared dependencie to webpack common

--- webpack.common.js
+++ webpack.common.js
@@ -1,76 +1,119 @@
 const path = require("path");
-const Handlebars = require('handlebars');
-const { TRUE } = require("node-sass");
+const webpack = require('webpack');
 const loader = require("sass-loader");
-
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 
 module.exports = {
-
      entry: {
               main: "./src/index.js",
-              vendor: "./src/vendor.js",
               home: './src/app/home.js',
               contact: './src/app/contact.js',
-              about: './src/app/about.js'
+              about: './src/app/about.js',
+              project: './src/app/projects',
+              vendor: "./src/vendor.js"
      },
      devtool: "source-map",
      module: {
        rules: [
+          {
+               test: /\.js$/,
+               exclude: /node_modules/,
+               use: {
+               loader: 'babel-loader',
+               options: {
+                  
+             }
+            }
+          },
             {
-                 test: /\.(png|svg|jpg|gif)$/,
+                 test: /\.(mp4|png|svg|jpg|gif|ogg)$/,
                  use: {
                       loader: 'file-loader',
                       options: {
-                         name: '[name].[hash].[ext]',
+                         name: '[name].[hash].[ext]'
+                         
                       }
-                 }
-            },
-            {
-                 test: /\.js$/,
-                 exclude: /node_modules/,
-                 use: {
-                 loader: 'babel-loader',
-                 options: {
-                    presets: ['@babel/preset-env']
-               }
-              }
-            }, 
-            {
-                 test: /\.html$/i,
-                 loader: 'html-loader',
-                      options: {
-                         minimize: true,
-                      }
-            },
-            {    test: /\.hbs$/,
-                 loader: 'html-loader',
-                      options: { 
-                           minimize: true,
-                           esModule: true,
-                           attributes: true,
-                           preprocessor: async (content, loaderContext) => {
-                                let result;
-                    
-                                try {
-                                  result = await Handlebars.compile(content)({
-                                    firstname: 'Value',
-                                    lastname: 'OtherValue'
-                                  });
-                                } catch (error) {
-                                  await loaderContext.emitError(error);
-                    
-                                  return content;
-                                }
-                    
-                                return result;
-                              }
-                         }
-                 }
+                    } 
+               },
+               {
+                    test: /\.html$/,
+                    loader: 'html-loader', 
+                         options: {
+                            minimize: true,
+                            attributes: true,
+                                                   
+                     }
+               },
+               {
+                  test: /\.(mp4|mp3|ogg)$/,
+                  use: 'file-loader?name=videos/[name].[ext]',
+           },
         ]
      },
+     optimization: {
+        minimize: true,
+         minimizer: [
+            new TerserJSPlugin({
+         test: /\.js(\?.*)?$/i,
+         parallel: true,
+      }), 
+      new OptimizeCSSAssetsPlugin({ cache: true })
+   ],
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks (chunk) {
+          // exclude `my-excluded-chunk`
+          return chunk.name !== 'my-excluded-chunk';
+        },
+        cacheGroups: {
+         vendor: {
+           test: /[\\/]node_modules[\\/]/,
+           name: 'vendors',
+           chunks: 'all',
+         },
+      }
+      }
+    },
+     plugins: [ 
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        React: 'react',
+        'react-dom': 'ReactDom'
+      }),
+       new HtmlWebpackPlugin({
+        filename: 'index.html',
+        inject: true,
+        template: './src/index.html',
+        minify: true,
+          
+      }),
+      new HtmlWebpackPlugin({
+       filename: 'contact.html',
+       template: './src/contact.html',
+       inject: true,
+       chunks: ['contact'],
+       minify: true
+     }),
+     new HtmlWebpackPlugin({
+      filename: 'about.html',
+      template: './src/about.html',
+      inject: true,
+      chunks: ['about'],
+      minify: true,
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'project.html',
+      template: './src/project.html',
+      inject: true,
+      chunks: ['project'],
+      minify: true,
+    }),
+  ],
      //To generate JSON file containing webpack records
      recordsPath: path.join(__dirname, 'records.json'),
      name: 'admin-app'
-  };
+};
