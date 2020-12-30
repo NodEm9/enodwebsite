dc2cb4540465332ec160666949d7001b3e99b94d
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2020-12-30 15:31:15 +0100

    add eventListener to social icons and updated Bio text

--- src/app/about.js
+++ src/app/about.js
@@ -1,10 +1,44 @@
+import bootstrap from 'bootstrap';
+
 import '../style/main.scss';
 import '../style/about.scss';
 
-export class AboutMe  {
-      constructor(){
-         
- }
+const AboutMe = () => {
+
 }
 
+const socialIcon = document.querySelector('.social__icon1');
 
+socialIcon.addEventListener('click',  function openGithubIcon(e) {
+            e.preventDefault();
+            if(socialIcon){
+                  window.open('https://github.com/NodEm9')
+            }
+});
+console.log(socialIcon);
+
+
+//Select the DOM element for Linkedin icon
+const linkedinIcon = document.querySelector('.social__icon2');
+
+linkedinIcon.addEventListener('click', function openIinkedinPage(e) {
+       e.preventDefault();
+
+       if(linkedinIcon){
+             window.open('https://www.linkedin.com/in/emmanuel-nodolomwanyi-60b56246');
+       } 
+ });
+
+ const para = document.querySelector('p');
+ para.setAttribute('id', 'bio-text' );
+ para.textContent = `Committed, dedicated, focued and full of fun, as well. I am a team player and 
+ also posses the skills and ability to work alone.
+  I like summer season because it's most convenient for my hobbies, 
+  i like riding bike as well as swimming in my free time amongst others.
+  Below are my skill set which I update reqularly `
+
+
+
+
+
+export { AboutMe };
