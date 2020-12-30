741de3e04c02531b9b303ee7a539eb378c20fe0d
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2020-12-30 15:33:13 +0100

    add eventListener to social icons

--- src/app/contact.js
+++ src/app/contact.js
@@ -1,23 +1,41 @@
 import { bootstrap } from 'bootstrap';
-import '../style/main.scss';
+
+import '../style/main.scss';  
 import '../style/contact.scss';
-import { openNewpage } from './home';
 
-const greetingMsg = document.querySelector('.greetingMsg');
 
+const cHeader = document.querySelector('.c-heading'); 
+          cHeader.textContent = 'Welcome to Contacts'; 
+          console.log(cHeader)
+         
 const contactPage = () => {
-
-          const cHeader = greetingMsg.querySelector('h1'); 
-          cHeader.textContent = 'Welcome to Contacts';
+          
           const cHeader1 = document.querySelector('.c-heading1');
-          cHeader1.textContent = `Reach us today, with any contact medium convinient to you\n 
-                                    In case we are not available at the moment, Kindly drop us a message
-                                    and we will  endeavour to get back to you ASAP. Thank you. `;                                    
- 
+          cHeader1.textContent = `Reach us today, with any contact medium convinient to you. 
+                    Call or email us and we will endeavour to get back to you ASAP. Thank you.`;
+
+                    console.log(cHeader1)         
 }
  contactPage();
 
+ //Select the DOM containing the GitHub icon 
+ //and store in a variable called socialIcon
+ const socialIcon = document.querySelector('.social__icon1');
+
+//Add a click event and a window.open function 
+//to enable the window open the page in a new tab
+ socialIcon.onclick = function () {
+          window.open('https://github.com/NodEm9') 
+ }
+ 
+ //selecting DOM for Linkedin icon
+ const linkedinIcon = document.querySelector('.social__icon2');
+
+ linkedinIcon.onclick = function () {
+          window.open('https://www.linkedin.com/in/emmanuel-nodolomwanyi-60b56246');
+ }
 
  
- export { contactPage }; 
+ //Export the contact page function 
+ export { contactPage };    
 
